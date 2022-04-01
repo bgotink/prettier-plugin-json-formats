27d5e50b958ddcb3900f0cd4721299d96ed9cf20
Bram Gotink <bram@gotink.me>
2022-04-01 12:14:07 +0200

    {feature} keep publishConfig.exports as is in package.json files

--- src/package-json.ts
+++ src/package-json.ts
@@ -58,80 +58,86 @@ const sortScripts = sortObjectProperties(node =>
   }),
 );
 
+const modifier = combine(
+  // Sort the top-level object in a given order
+  sortObjectProperties([
+    'name',
+    'version',
+    'private',
+
+    'description',
+    'keywords',
+    'homepage',
+    'bugs',
+    'repository',
+    'license',
+    'author',
+    'contributors',
+
+    'bin',
+    'man',
+    'directovires',
+    'files',
+    'sideEffects',
+
+    'workspaces',
+    'scripts',
+
+    'main',
+    'exports',
+    'imports',
+
+    'umd:main',
+    'jsdelivr',
+    'unpkg',
+    'module',
+    'source',
+    'jsnext:main',
+    'browser',
+    'types',
+    'typings',
+    'style',
+
+    'dependencies',
+    'bundledDependencies',
+    'bundleDependencies',
+    'optionalDependencies',
+    'peerDependencies',
+    'peerDependenciesMeta',
+    'devDependencies',
+    'dependenciesMeta',
+
+    'engines',
+    'publishConfig',
+  ]),
+
+  // Now sort the values of the top-level properties
+  replacePropertyValues((value, opts, key) => {
+    switch (key) {
+      case 'exports':
+      case 'imports':
+        // Keep exports and imports as they are. Order can be important here,
+        // depending on the shape of the declared exports/imports.
+        return value;
+
+      case 'scripts':
+        // Sort scripts using a script-specific sort order
+        return sortScripts(value, opts);
+
+      case 'bundleDependencies':
+      case 'bundledDependencies':
+        return sortStringArray(value, opts);
+
+      case 'publishConfig':
+        return modifier(value, opts);
+
+      default:
+        return deepSortObjectProperties()(value, opts);
+    }
+  }),
+);
+
 export const packageJsonPlugin = createJsonPlugin({
   language: {name: 'package-json'},
-  modifier: combine(
-    // Sort the top-level object in a given order
-    sortObjectProperties([
-      'name',
-      'version',
-      'private',
-
-      'description',
-      'keywords',
-      'homepage',
-      'bugs',
-      'repository',
-      'license',
-      'author',
-      'contributors',
-
-      'bin',
-      'man',
-      'directovires',
-      'files',
-      'sideEffects',
-
-      'workspaces',
-      'scripts',
-
-      'main',
-      'exports',
-
-      'umd:main',
-      'jsdelivr',
-      'unpkg',
-      'module',
-      'source',
-      'jsnext:main',
-      'browser',
-      'types',
-      'typings',
-      'style',
-
-      'dependencies',
-      'bundledDependencies',
-      'bundleDependencies',
-      'optionalDependencies',
-      'peerDependencies',
-      'peerDependenciesMeta',
-      'devDependencies',
-      'dependenciesMeta',
-
-      'engines',
-      'publishConfig',
-    ]),
-
-    // Now sort the values of the top-level properties
-    replacePropertyValues((value, opts, key) => {
-      switch (key) {
-        case 'exports':
-        case 'imports':
-          // Keep exports and imports as they are. Order can be important here,
-          // depending on the shape of the declared exports/imports.
-          return value;
-
-        case 'scripts':
-          // Sort scripts using a script-specific sort order
-          return sortScripts(value, opts);
-
-        case 'bundleDependencies':
-        case 'bundledDependencies':
-          return sortStringArray(value, opts);
-
-        default:
-          return deepSortObjectProperties()(value, opts);
-      }
-    }),
-  ),
+  modifier,
 });
